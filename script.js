6e2e51fe8b12b2d86080a2459c24b84491a870ad
Sakshi K.C. <141574185+acker111@users.noreply.github.com>
2024-03-26 22:07:51 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const countEl= document.getElementById('count-el')
+
+console.log(countEl)
+
+let count=0
+function increment(){
+    count=count+1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+
+
+
+
